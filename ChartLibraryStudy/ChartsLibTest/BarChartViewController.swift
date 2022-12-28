4c7e31aa7150765a3a466d3d790a286c67bae0cf
김지태 <wlxo0401@gmail.com>
2022-12-28 16:29:46 +0900

    그래프 라이브러리

---
+++ ChartLibraryStudy/ChartsLibTest/BarChartViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  ChartsLibTest
+//
+//  Created by 김지태 on 2022/11/07.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    @IBOutlet weak var myBarChartView: BarChartView!
+    
+    
+    var dayData: [String] = ["11월02일", "11월03일", "11월04일", "11월05일", "11월06일", "11월07일", "11월08일", "11월09일", "11월10일"]
+    var priceData: [Double]! = [100, 345, 20, 120, 90, 300, 450, 220, 120]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // 데이터가 없을 때 보일 글씨
+        self.myBarChartView.noDataText = "출력 데이터가 없습니다."
+        // 데이터가 없을 때 보일 글씨 폰트
+        self.myBarChartView.noDataFont = .systemFont(ofSize: 20)
+        // 데이터가 없을 때 보일 글씨 색상
+        self.myBarChartView.noDataTextColor = .red
+        // 데이터가 없을 때 보일 글씨 폰트 정렬
+        self.myBarChartView.noDataTextAlignment = .left
+        
+        
+        self.myBarChartView.backgroundColor = .white
+        
+        
+        // 데이터 범례 form 모양
+        self.myBarChartView.legend.form = .square
+        
+        // x축 Grid
+        self.myBarChartView.xAxis.drawGridLinesEnabled = true
+        // x축 라벨
+        self.myBarChartView.xAxis.drawLabelsEnabled = true
+        // x축 선
+        self.myBarChartView.xAxis.drawAxisLineEnabled = true
+        
+        
+        // 데이터 범례 삭제
+        self.myBarChartView.legend.enabled = false
+        
+        
+        self.myBarChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dayData)
+        self.myBarChartView.xAxis.setLabelCount(priceData.count, force: false)
+        self.setBarData(barChartView: self.myBarChartView, barChartDataEntries: self.entryData(values: self.priceData))
+    }
+
+
+    func setBarData(barChartView: BarChartView, barChartDataEntries: [BarChartDataEntry]) {
+        let barChartdataSet = BarChartDataSet(entries: barChartDataEntries, label: "매출")
+
+        
+        let barChartData = BarChartData(dataSet: barChartdataSet)
+        barChartView.data = barChartData
+    }
+    
+    // entry 만들기
+    func entryData(values: [Double]) -> [BarChartDataEntry] {
+        var barDataEntries: [BarChartDataEntry] = []
+        for i in 0 ..< values.count {
+            let barDataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            barDataEntries.append(barDataEntry)
+        }
+        return barDataEntries
+    }
+}
+
---
+++ ChartLibraryStudy/ChartsLibTest/PieChartViewController.swift
@@ -0,0 +1,68 @@
+//
+//  PieChartViewController.swift
+//  ChartsLibTest
+//
+//  Created by 김지태 on 2022/11/15.
+//
+
+import UIKit
+import Charts
+
+class PieChartViewController: UIViewController {
+
+    
+    @IBOutlet weak var myPieChart: PieChartView!
+    
+    var dayData: [String] = ["11월02일", "11월03일", "11월04일", "11월05일", "11월06일", "11월07일", "11월08일", "11월09일", "11월10일"]
+    var priceData: [Double]! = [100, 345, 20, 120, 90, 300, 450, 220, 120]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let value = 10.0
+        print(sqrt(value))
+        
+        
+        
+        // 기본 출력 텍스트
+        self.myPieChart.noDataText = "출력 데이터가 없습니다."
+        // 기본 출력 텍스트 폰트
+        self.myPieChart.noDataFont = .systemFont(ofSize: 20)
+        // 기본 출력 텍스트 색상
+        self.myPieChart.noDataTextColor = .lightGray
+        // Chart 뒷 배경 색상
+        self.myPieChart.backgroundColor = .white
+        
+        
+        self.setPieData(pieChartView: self.myPieChart, pieChartDataEntries: self.entryData(values: self.priceData))
+        
+    }
+    
+    // 데이터 적용하기
+    func setPieData(pieChartView: PieChartView, pieChartDataEntries: [ChartDataEntry]) {
+        // Entry들을 이용해 Data Set 만들기
+        let pieChartdataSet = PieChartDataSet(entries: pieChartDataEntries, label: "매출")
+        // DataSet을 차트 데이터로 넣기
+        let pieChartData = PieChartData(dataSet: pieChartdataSet)
+        // 데이터 출력
+        pieChartView.data = pieChartData
+    }
+
+    // entry 만들기
+    func entryData(values: [Double]) -> [ChartDataEntry] {
+        // entry 담을 array
+        var pieDataEntries: [ChartDataEntry] = []
+        // 담기
+        for i in 0 ..< values.count {
+            let pieDataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            pieDataEntries.append(pieDataEntry)
+        }
+        // 반환
+        return pieDataEntries
+    }
+
+}
+
+
+
