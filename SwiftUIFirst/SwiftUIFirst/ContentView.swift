39663b050c1c07296aafcf39271bad82944410b8
김지태 <wlxo0401@gmail.com>
2023-02-22 22:19:08 +0900

    Update ContentView.swift

--- SwiftUIFirst/SwiftUIFirst/ContentView.swift
+++ SwiftUIFirst/SwiftUIFirst/ContentView.swift
@@ -9,42 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var isShow: Bool = false
-    @State var backgrounddd: UIColor = .red
-    
-    @State var content: String = ""
+    @State var SelectedColor: Color = .white
     
     var body: some View {
         
-        VStack {
-            VStack(alignment: .leading) {
-                Text("아이디를 입력하세요.")
-                    .padding()
-                TextField("ID", text: $content)
-                    .padding()
-                    .textFieldStyle(.roundedBorder)
-                    
-            }
-            
-            Button {
-                self.isShow = true
-            } label: {
-                Text("아이디 확인")
-            }
-            
-            .alert("입력된 ID", isPresented: $isShow, actions: {
-                Button {
-                    self.isShow = false
-                } label: {
-                    Text("확인")
-                }
-            }, message: {
-                Text(self.content)
-            })
-            
+        
+        
+        List {
+            Text("Hi")
         }
-        
-        
     }
 }
 
