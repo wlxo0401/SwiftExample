699f85b49109123e5ea594b69d83eb4a25e975d8
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2023-11-06 21:48:22 +0900

    Alamofire 예제 추가

--- AlamofireEx/AlamofireEx/ViewController.swift
+++ AlamofireEx/AlamofireEx/ViewController.swift
@@ -23,8 +23,39 @@ class ViewController: UIViewController {
         
         let url: String = "https://jsonplaceholder.typicode.com/todos"
         
-        AF.request(url,
-                   method: .get)
+        let headers: HTTPHeaders = ["Content-Type": "application/json",
+                                    "Accept": "application/json"]
+        
+        // Body 데이터
+        let parameters: Parameters = ["userId": "swift",
+                                      "name": "kimjitae",
+                                      "amount": 1000,
+                                      "age": 27]
+        
+        // 쿼리 데이터
+        let queryParam: Parameters = ["game": "KartRider"]
+        
+        // Request 생성
+        var request: URLRequest = URLRequest(url: URL(string: url)!)
+        // Method 설정
+        request.httpMethod = HTTPMethod.post.rawValue
+        // 헤더
+        request.headers = headers
+        
+        // 파라메터 Json String 변환
+        if let location = self.dictionaryToJsonString(dictionary: parameters) {
+            // here body is already serialized to Json
+            request.httpBody = Data(location.utf8)
+        }
+        
+        do {
+            // 쿼리 데이터 추가
+            request = try URLEncoding(destination: .queryString).encode(request, with: queryParam)
+        } catch {
+            print("Error creating request: \(error)")
+        }
+        
+        AF.request(request)
                     .validate(statusCode: 200..<300)
                     .validate(contentType: ["application/json"])
                     .responseDecodable(of: [TestModel].self) { (response) in
@@ -35,6 +66,18 @@ class ViewController: UIViewController {
                             print("통신 에러 \(error.localizedDescription)")
                         }
                     }
+    }
+    
+    func dictionaryToJsonString(dictionary: [String: Any]) -> String? {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: dictionary, options: [])
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                return jsonString
+            }
+        } catch {
+            print("Error converting dictionary to JSON string: \(error)")
+        }
+        return nil
     }
 }
 
