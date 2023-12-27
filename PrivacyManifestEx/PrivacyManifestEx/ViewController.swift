34683f15037f5acb40ae4d903d02a9e63cfd474b
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2023-12-27 13:51:48 +0900

    Privacy Manifest Test

---
+++ PrivacyManifestEx/PrivacyManifestEx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrivacyManifestEx
+//
+//  Created by 김지태 on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
